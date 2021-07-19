5fa02f290b33b240e27663e5a53758139f68de19
Ivan Smirnov <ivsmir@gmail.com>
2021-07-19 12:20:19 +0300

    Initial commit

---
+++ pgfreeze-qemu.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+
+# Default values
+PGUSER="postgres"
+LABEL="pgfreeze"
+STARTASAP=1
+
+[ -r /etc/default/pgfreeze ] && . /etc/default/pgfreeze
+
+case "$1" in
+freeze)
+	if [ "$STARTASAP" -eq 1 ]; then
+		SQL="SELECT pg_start_backup('${LABEL}', true);"
+	else
+		SQL="SELECT pg_start_backup('${LABEL}');"
+	fi
+	
+	su -c "echo \"$SQL\" | psql" - ${PGUSER}
+	logger "Postgres has been freezed for backup"
+	;;
+thaw)
+	SQL="SELECT * FROM pg_stop_backup();"
+	su -c "echo \"$SQL\" | psql" - ${PGUSER}
+	logger "Postgress has been unfrezed"
+        ;;
+*)
+	echo "Usage: ./pgfreeze.sh (freeze|thaw)"
+        exit 1
+        ;;
+esac
+exit 0
